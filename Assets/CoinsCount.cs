717ec8e0dbdb54163b130db3d06b9858a264b047
shala <mohamedkhaled.shala@mail.polimi.it>
2023-11-27 00:20:49 +0100

    adding timer and change scenes when all coins are collected or time is finished

--- Assets/CoinsCount.cs
+++ Assets/CoinsCount.cs
@@ -2,18 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CoinsCount : MonoBehaviour
-{
+{   
+    public string nextScene;
     public int maxCoins;
+    
+    void Start()
+    {
+        TextMeshProUGUI textMesh = GetComponent<TextMeshProUGUI>();
+        textMesh.text = HideWhenClose.globalCoins.ToString();
+    } 
 
     // Update is called once per frame
+    
     void Update()
     {
         TextMeshProUGUI textMesh = GetComponent<TextMeshProUGUI>();
         textMesh.text = HideWhenClose.globalCoins.ToString();
         if (HideWhenClose.globalCoins >= maxCoins) {
             textMesh.text = "Well done! You finished all the coins";
+            //we should add delay here 
+            SceneManager.LoadScene(nextScene);
         }
     }
 }
