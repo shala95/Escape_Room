953925188a4dbde32287e602b6cdb2d00990d41f
Valentina <valentina.lomonte@mail.polimi.it>
2023-11-27 17:28:13 +0100

    Add change text from one level to another and fixed cross to quest

--- Assets/Point/ScoreManager.cs
+++ Assets/Point/ScoreManager.cs
@@ -11,22 +11,34 @@ public class ScoreManager : MonoBehaviour
     public void OnButtonClick(string input)
     {
         
-            if (input.Contains("Never"))
-            {
-                globalScore += 1;
-            }
-            else if (input.Contains("SomeDay"))
-            {
-                globalScore += 2;
-            }
-            else if (input.Contains("Always"))
-            {
-                globalScore += 3;
-            }
+        if (input.Contains("Never"))
+        {
+            globalScore += 1;
+        }
+        else if (input.Contains("SomeDay"))
+        {
+            globalScore += 2;
+        }
+        else if (input.Contains("Always"))
+        {
+            globalScore += 3;
+        }
 
         
 
-        SceneManager.LoadScene(input.Split(" ")[1]); //this code splits the string at the space and returns the scene name
+        //SceneManager.LoadScene(input.Split(" ")[1]); //this code splits the string at the space and returns the scene name
+        if (SceneManager.GetActiveScene().name == "Quest1"){
+            SceneManager.LoadScene("Quest2");
+        }
+        else if (SceneManager.GetActiveScene().name == "Quest2"){
+            SceneManager.LoadScene("Quest3");
+        }
+        else if (SceneManager.GetActiveScene().name == "Quest3"){
+            SceneManager.LoadScene("Quest4");
+        }
+        else if (SceneManager.GetActiveScene().name == "Quest4"){
+            SceneManager.LoadScene("AYR");
+        }
     }
 }
 
