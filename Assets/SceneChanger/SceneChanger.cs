64d64085ba42a712927eeb9f5e9fef1582f022f7
Davide Chiarappa <davide.chiarappa@mail.polimi.it>
2023-12-05 15:42:01 +0100

    Modifing level 2.
    Add pusic pt. 3
    Add obstacles but work in progress beacause in my opinion we have to change something

--- Assets/SceneChanger/SceneChanger.cs
+++ Assets/SceneChanger/SceneChanger.cs
@@ -7,25 +7,44 @@ public class SceneChanger : MonoBehaviour
 {
     public void ChangeScene(string sceneName)
 	{	
-		if(sceneName=="Mainpage")
-		{
-			ScoreManager.globalScore=0;
-		}	
 		SceneManager.LoadScene (sceneName);
-		MusicManager musicManager = Object.FindAnyObjectByType<MusicManager>();
+		MusicManager musicManager = Object.FindFirstObjectByType<MusicManager>();
+		AudioSource audioSource = musicManager.GetComponent<AudioSource>();
 		
 		if (musicManager != null)
 		{
-			// Se la musica non è in riproduzione, ricomincia dall'inizio
-			if (!musicManager.GetComponent<AudioSource>().isPlaying)
+			if(sceneName=="Mainpage")
 			{
-				musicManager.GetComponent<AudioSource>().Play();
+				ScoreManager.globalScore=0;
+			
+				if (!audioSource.isPlaying)
+				{
+					audioSource.Play();
+				}
+				else
+				{
+					audioSource.UnPause();
+				}
 			}
-    
+			
+			if(sceneName=="Intro" || sceneName=="IntroQuest" || sceneName=="Quest1" || sceneName=="Quest2" || sceneName=="Quest3" || sceneName=="Quest4" || sceneName=="AYR" )
+			{
+				audioSource.UnPause();
+			}
+			
+			if(sceneName=="Level_1"  || sceneName=="Level_2" || sceneName=="Level_3")
+			{
+				audioSource.Pause();
+			}
+
+			if (sceneName=="ChangeScene12" || sceneName=="ChangeScene12Failed" || sceneName=="ChangeScene23" || sceneName=="ChangeScene23Failed" || sceneName=="Final" || sceneName=="FinalFailed")
+			{
+				audioSource.UnPause();
+			}
+			
 			// Assicura che l'oggetto MusicManager persista durante il cambio di scena
 			DontDestroyOnLoad(musicManager.gameObject);
 		}
-		
 	}
 	public void Exit()
 	{
